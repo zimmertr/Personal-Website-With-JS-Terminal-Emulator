4f521004e4b8b80e9029ffc52cd4e1bed542f250
TJ Zimmerman <tj@tjzimmerman.com>
2018-04-15 11:22:34 -0700

    updated getweather script to export output to proper location.

--- cgi-bin/getWeather.sh
+++ cgi-bin/getWeather.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 /usr/bin/weatherpy | /usr/bin/head -n -1 > /var/www/cgi-bin/weather.txt 2>&1
-/usr/bin/weatherpy -feat civilian >> weather.txt
+/usr/bin/weatherpy -feat civilian >> /var/www/cgi-bin/weather.txt
